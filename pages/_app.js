a6ef39c5062b779e00277e2177e0dc4c73e496e0
KingArthvr_ <33429259+TheKingArthvr@users.noreply.github.com>
2022-06-15 11:40:24 +0200

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -4,4 +4,37 @@ function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default Application
+import { ThemeProvider } from '@emotion/react'
+// _app.js
+import App from 'next/app'
+
+// _app.js
+import withDarkMode from 'next-dark-mode'
+
+function MyApp(props) {
+  const {
+    autoModeActive,
+    autoModeSupported,
+    darkModeActive,
+    switchToAutoMode,
+    switchToDarkMode,
+    switchToLightMode,
+  } = props.darkMode
+}
+function MyApp({ Component, darkMode, pageProps }) {
+  const {
+    autoModeActive,
+    autoModeSupported,
+    darkModeActive,
+    switchToAutoMode,
+    switchToDarkMode,
+    switchToLightMode,
+  } = darkMode
+
+  return (
+    <ThemeProvider theme={{ darkMode: darkModeActive}}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+  export default withDarkMode(MyApp)
